2c5138212e9f2d27216b24d0708e51b7e203e1e1
binio <tbiniecki@gmail.com>
2018-02-16 20:00:00 -0600

    add Recipe repo

--- src/main/java/com/binio/recipes/repositories/RecipeRepository.java
+++ src/main/java/com/binio/recipes/repositories/RecipeRepository.java
@@ -1,4 +1,7 @@
 package com.binio.recipes.repositories;
 
-public interface RecipeRepository {
+import com.binio.recipes.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 }
