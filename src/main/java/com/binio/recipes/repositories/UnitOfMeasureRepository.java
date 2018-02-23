ac712899bad0135ec8df08830273e6b0ec07ed2d
binio <tbiniecki@gmail.com>
2018-02-23 23:13:36 +0000

    add query method

--- src/main/java/com/binio/recipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/binio/recipes/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.binio.recipes.repositories;
 import com.binio.recipes.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    Optional<UnitOfMeasure> findByOum(String oum);
 }
