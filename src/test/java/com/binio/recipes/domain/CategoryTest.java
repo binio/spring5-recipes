fe954f093ee295d9670cc7967f26edd7f03dde88
binio <tbiniecki@gmail.com>
2018-02-25 23:47:37 +0000

    category test

--- src/test/java/com/binio/recipes/domain/CategoryTest.java
+++ src/test/java/com/binio/recipes/domain/CategoryTest.java
@@ -1,12 +1,21 @@
 package com.binio.recipes.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CategoryTest {
 
+    private Category category;
+
+    @Before
+    public void setup(){
+        category = new Category();
+        category.setDescription("Mexican");
+    }
     @Test
     public void setDescription() {
+        assertEquals("Mexican",category.getDescription());
     }
 }
