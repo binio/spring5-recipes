c6cd73753c89855febfe893ca1bc862bdb80da68
binio <tbiniecki@gmail.com>
2018-02-25 22:55:52 +0000

    category test

---
+++ src/test/java/com/binio/recipes/domain/CategoryTest.java
@@ -0,0 +1,12 @@
+package com.binio.recipes.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    @Test
+    public void setDescription() {
+    }
+}
