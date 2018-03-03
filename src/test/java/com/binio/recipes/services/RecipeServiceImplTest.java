e1e1725ccce9d1406c9d156316c877318d6184ae
binio <tbiniecki@gmail.com>
2018-03-03 23:39:50 +0000

    serviceImplementation test

---
+++ src/test/java/com/binio/recipes/services/RecipeServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.binio.recipes.services;
+
+import com.binio.recipes.repositories.RecipeRepository;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class RecipeServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Test
+    public void getAllRecipes() {
+    }
+
+    @Test
+    public void findById() {
+    }
+}
