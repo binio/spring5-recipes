e1e356cca29332735d4ddc93ee3302c9056704e3
binio <tbiniecki@gmail.com>
2018-03-05 23:11:37 +0000

    add Tests

---
+++ src/test/java/com/binio/recipes/converters/CategoryToCategoryCommandTest.java
@@ -0,0 +1,12 @@
+package com.binio.recipes.converters;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryToCategoryCommandTest {
+
+    @Test
+    public void convert() {
+    }
+}
