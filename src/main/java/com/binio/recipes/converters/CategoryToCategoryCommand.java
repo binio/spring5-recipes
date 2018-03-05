3f5fa397499466d189013da53a039de07126ef91
binio <tbiniecki@gmail.com>
2018-03-05 23:11:23 +0000

    add categoryCommand converter

--- src/main/java/com/binio/recipes/converters/CategoryToCategoryCommand.java
+++ src/main/java/com/binio/recipes/converters/CategoryToCategoryCommand.java
@@ -2,12 +2,16 @@ package com.binio.recipes.converters;
 
 import com.binio.recipes.commands.CategoryCommand;
 import com.binio.recipes.domain.Category;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoryToCategoryCommand implements Converter<CategoryCommand, Category> {
 
+    @Synchronized
+    @Nullable
     @Override
     public Category convert(CategoryCommand categoryCommand) {
 
@@ -15,7 +19,7 @@ public class CategoryToCategoryCommand implements Converter<CategoryCommand, Cat
             return null;
         }
 
-        Category category = new Category();
+        final Category category = new Category();
         category.setId(categoryCommand.getId());
         category.setDescription(categoryCommand.getDescription());
 
