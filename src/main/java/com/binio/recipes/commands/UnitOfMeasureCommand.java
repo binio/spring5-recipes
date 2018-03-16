db02071b8f2e90feccff06c91d48536e2dae62c2
binio <tbiniecki@gmail.com>
2018-03-15 20:00:00 -0600

    add UnitOfMeasureCommand

---
+++ src/main/java/com/binio/recipes/commands/UnitOfMeasureCommand.java
@@ -0,0 +1,14 @@
+package com.binio.recipes.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitOfMeasureCommand {
+
+    private Long id;
+    private String oum;
+}
