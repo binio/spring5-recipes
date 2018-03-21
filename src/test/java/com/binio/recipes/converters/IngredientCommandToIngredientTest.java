36b79e9bf626184a6de9c976dcefac7638c0efe5
binio <tbiniecki@gmail.com>
2018-03-21 00:06:53 +0000

    add IngredientConverterTest

---
+++ src/test/java/com/binio/recipes/converters/IngredientCommandToIngredientTest.java
@@ -0,0 +1,12 @@
+package com.binio.recipes.converters;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class IngredientCommandToIngredientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
