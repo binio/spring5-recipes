9dd24e4f682f59954b90900149188ca7e3581486
binio <tbiniecki@gmail.com>
2018-03-28 20:00:00 -0600

    add converter

--- src/main/java/com/binio/recipes/converters/NotesCommandToNotes.java
+++ src/main/java/com/binio/recipes/converters/NotesCommandToNotes.java
@@ -1,4 +1,25 @@
 package com.binio.recipes.converters;
 
-public class NotesCommandToNotes {
+import com.binio.recipes.commands.NotesCommand;
+import com.binio.recipes.domain.Notes;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class NotesCommandToNotes implements Converter<NotesCommand, Notes> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Notes convert(NotesCommand nc) {
+        if(nc == null) {
+            return null;
+        }
+
+        Notes notes = new Notes();
+        nc.setId(nc.getId());
+        nc.setRecipeNotes(nc.getRecipeNotes());
+
+        return notes;
+    }
 }
