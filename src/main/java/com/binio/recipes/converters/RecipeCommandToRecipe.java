b3ac49e63c16523a3aa769826f340d074602c488
binio <tbiniecki@gmail.com>
2018-03-29 20:00:00 -0600

    add converter

--- src/main/java/com/binio/recipes/converters/RecipeCommandToRecipe.java
+++ src/main/java/com/binio/recipes/converters/RecipeCommandToRecipe.java
@@ -1,4 +1,60 @@
 package com.binio.recipes.converters;
 
-public class RecipeCommandToRecipe {
+import com.binio.recipes.commands.CategoryCommand;
+import com.binio.recipes.commands.RecipeCommand;
+import com.binio.recipes.domain.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
+
+    private CategoryCommandToCategory categoryCommand;
+    private IngredientCommandToIngredient ingredientCommand;
+    private NotesCommandToNotes notesCommand;
+
+    public RecipeCommandToRecipe(
+            CategoryCommandToCategory categoryCommand,
+            IngredientCommandToIngredient ingredientCommand,
+            NotesCommandToNotes notesCommand) {
+
+        this.categoryCommand = categoryCommand;
+        this.ingredientCommand = ingredientCommand;
+        this.notesCommand = notesCommand;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand rc) {
+        if(rc == null){
+            return null;
+        }
+
+        Recipe recipe = new Recipe();
+        recipe.setId(rc.getId());
+        recipe.setDescription(rc.getDescription());
+        recipe.setDifficulty(rc.getDifficulty());
+        recipe.setServings(rc.getServings());
+        recipe.setPrepTime(rc.getPrepTime());
+        recipe.setDirections(rc.getDirections());
+        recipe.setDifficulty(rc.getDifficulty());
+        recipe.setCookTime(rc.getCookTime());
+
+        if(rc.getCategories() != null){
+            for (CategoryCommand cc: rc.getCategories()) {
+                recipe.getCategories().add(categoryCommand.convert(cc));
+            }
+        }
+
+        if(rc.getIngredients() !=null){
+            rc.getIngredients().forEach(i -> recipe.getIngredients().add(ingredientCommand.convert(i)));
+        }
+
+        if(rc.getNotes() != null){
+            recipe.setNotes(notesCommand.convert(rc.getNotes()));
+        }
+
+        return recipe;
+    }
 }
