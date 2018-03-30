2509a779b433ccd8f4abb35db718316816a4e4f6
binio <tbiniecki@gmail.com>
2018-03-29 20:00:00 -0600

    add converter

--- src/main/java/com/binio/recipes/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/com/binio/recipes/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -1,4 +1,21 @@
 package com.binio.recipes.converters;
 
-public class UnitOfMeasureCommandToUnitOfMeasure {
+import com.binio.recipes.commands.UnitOfMeasureCommand;
+import com.binio.recipes.domain.UnitOfMeasure;
+import org.springframework.core.convert.converter.Converter;
+
+public class UnitOfMeasureCommandToUnitOfMeasure  implements Converter<UnitOfMeasureCommand,UnitOfMeasure> {
+
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand unitOfMeasureCommand) {
+        if(unitOfMeasureCommand == null) {
+            return null;
+        }
+
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setOum(unitOfMeasure.getOum());
+        unitOfMeasure.setId(unitOfMeasure.getId());
+
+        return unitOfMeasure;
+    }
 }
