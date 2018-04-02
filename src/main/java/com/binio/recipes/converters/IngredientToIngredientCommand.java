d4a859d79d0b316a9b2d2d21d44d5415815dcea8
binio <tbiniecki@gmail.com>
2018-04-02 09:55:49 +0100

    add convert Ingredient converter

---
+++ src/main/java/com/binio/recipes/converters/IngredientToIngredientCommand.java
@@ -0,0 +1,26 @@
+package com.binio.recipes.converters;
+
+import com.binio.recipes.commands.IngredientCommand;
+import com.binio.recipes.domain.Ingredient;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class IngredientToIngredientCommand implements Converter<Ingredient, IngredientCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public IngredientCommand convert(Ingredient ingredient) {
+        if(ingredient == null) {
+            return null;
+        }
+        IngredientCommand ic = new IngredientCommand();
+        ic.setId(ingredient.getId());
+        ic.setDescription(ingredient.getDescription());
+        ic.setAmout(ingredient.getAmout());
+        ic.setUnitMeasure(ingredient.getUnitMeasure());
+
+        return ic;
+    }
+}
