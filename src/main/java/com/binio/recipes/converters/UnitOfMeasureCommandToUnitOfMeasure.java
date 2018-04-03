d6c958e690358a79e6210364a4803101868c9ca0
binio <tbiniecki@gmail.com>
2018-04-03 23:56:04 +0100

    add annotations

--- src/main/java/com/binio/recipes/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/com/binio/recipes/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -2,10 +2,16 @@ package com.binio.recipes.converters;
 
 import com.binio.recipes.commands.UnitOfMeasureCommand;
 import com.binio.recipes.domain.UnitOfMeasure;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UnitOfMeasureCommandToUnitOfMeasure  implements Converter<UnitOfMeasureCommand,UnitOfMeasure> {
 
+    @Synchronized
+    @Nullable
     @Override
     public UnitOfMeasure convert(UnitOfMeasureCommand unitOfMeasureCommand) {
         if(unitOfMeasureCommand == null) {
