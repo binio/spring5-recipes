06b899c08b21b1ca01f2a07c9a88f57ad0ef9718
binio <tbiniecki@gmail.com>
2018-04-03 23:55:30 +0100

    add annotations

--- src/main/java/com/binio/recipes/converters/CategoryToCategoryCommand.java
+++ src/main/java/com/binio/recipes/converters/CategoryToCategoryCommand.java
@@ -2,11 +2,15 @@ package com.binio.recipes.converters;
 
 import com.binio.recipes.commands.CategoryCommand;
 import com.binio.recipes.domain.Category;
+import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 
 public class CategoryToCategoryCommand implements Converter<Category, CategoryCommand>{
 
 
+    @Synchronized
+    @Nullable
     @Override
     public CategoryCommand convert(Category category) {
         if (category == null) {
