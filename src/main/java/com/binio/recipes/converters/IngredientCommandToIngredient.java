faf18153c654152603b735ab94f14bf501504079
binio <tbiniecki@gmail.com>
2018-04-06 22:56:10 +0100

    fix converter

--- src/main/java/com/binio/recipes/converters/IngredientCommandToIngredient.java
+++ src/main/java/com/binio/recipes/converters/IngredientCommandToIngredient.java
@@ -1,7 +1,9 @@
 package com.binio.recipes.converters;
 
 import com.binio.recipes.commands.IngredientCommand;
+import com.binio.recipes.commands.UnitOfMeasureCommand;
 import com.binio.recipes.domain.Ingredient;
+import com.binio.recipes.domain.UnitOfMeasure;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
@@ -15,12 +17,15 @@ public class IngredientCommandToIngredient implements Converter<IngredientComman
     @Override
     public Ingredient convert(IngredientCommand ingredientCommand) {
 
+        UnitOfMeasureCommandToUnitOfMeasure unitOfMeasureCommandToUnitOfMeasure = new UnitOfMeasureCommandToUnitOfMeasure();
+
         if(ingredientCommand == null) {
             return null;
         }
+
         Ingredient ingredient = new Ingredient();
         ingredient.setDescription(ingredientCommand.getDescription());
-        ingredient.setUnitMeasure(ingredientCommand.getUnitMeasure());
+        ingredient.setUnitMeasure(unitOfMeasureCommandToUnitOfMeasure.convert(ingredientCommand.getUnitOfMeasureCommand()));
         ingredient.setAmout(ingredientCommand.getAmout());
         ingredient.setId(ingredientCommand.getId());
         return ingredient;
