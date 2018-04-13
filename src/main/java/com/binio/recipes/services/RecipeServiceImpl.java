6fe0abdb51597d63527f63a71e9c6e48ce90fb4f
binio <tbiniecki@gmail.com>
2018-04-12 20:00:00 -0600

    save recipe command

--- src/main/java/com/binio/recipes/services/RecipeServiceImpl.java
+++ src/main/java/com/binio/recipes/services/RecipeServiceImpl.java
@@ -1,20 +1,28 @@
 package com.binio.recipes.services;
 
+import com.binio.recipes.commands.RecipeCommand;
+import com.binio.recipes.converters.RecipeCommandToRecipe;
+import com.binio.recipes.converters.RecipeToRecipeCommand;
 import com.binio.recipes.domain.Recipe;
 import com.binio.recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private  final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, final RecipeCommandToRecipe recipeCommandToRecipe, final RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     public Set<Recipe> getAllRecipes() {
@@ -31,4 +39,12 @@ public class RecipeServiceImpl implements RecipeService{
 
         return recipe.get();
     }
+
+    public RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(recipeCommand);
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved recipeID: " + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
+    }
 }
+
