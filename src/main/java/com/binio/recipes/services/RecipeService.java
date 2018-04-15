f134562b9feb168a71a8e770e85b2aaefff45d1a
binio <tbiniecki@gmail.com>
2018-04-14 20:00:00 -0600

    add method to interface

--- src/main/java/com/binio/recipes/services/RecipeService.java
+++ src/main/java/com/binio/recipes/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.binio.recipes.services;
 
+import com.binio.recipes.commands.RecipeCommand;
 import com.binio.recipes.domain.Recipe;
 
 import java.util.Set;
@@ -7,4 +8,6 @@ import java.util.Set;
 public interface RecipeService {
     Set<Recipe> getAllRecipes();
     Recipe findById(Long id);
+    RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
+
 }
