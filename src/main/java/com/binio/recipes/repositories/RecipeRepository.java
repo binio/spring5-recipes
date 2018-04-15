b9b07fa510041922ba592e0f71fe32a315dc80fd
binio <tbiniecki@gmail.com>
2018-04-14 20:00:00 -0600

    add annotation

--- src/main/java/com/binio/recipes/repositories/RecipeRepository.java
+++ src/main/java/com/binio/recipes/repositories/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.binio.recipes.repositories;
 
 import com.binio.recipes.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 }
